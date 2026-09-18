1e1f24497f805f1f57d74731eb3cb21254e964e7
fgl161107-hub <fgl161107@gmail.com>
2026-09-18 12:32:14 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+window.SAGE_DATA={
+  water:{
+    title:'Siklus Air', subtitle:'Menelusuri perjalanan air dari bumi ke atmosfer dan kembali lagi.', accent:'water',
+    stages:['Observasi','Telusuri','Laboratorium','Simulasi Cuaca','Diagnosis','Misi Lapangan','Refleksi'],
+    hero:'https://commons.wikimedia.org/wiki/Special:FilePath/Sea%2C%20Indonesia.jpg?width=1400',
+    images:{
+      sea:'https://commons.wikimedia.org/wiki/Special:FilePath/Sea%2C%20Indonesia.jpg?width=1200',
+      lake:'https://commons.wikimedia.org/wiki/Special:FilePath/Ngebel%20lake.jpg?width=1200',
+      river:'https://commons.wikimedia.org/wiki/Special:FilePath/River%20and%20stream%20in%20Bogor%2C%20West%20Java%2C%20Indonesia.jpg?width=1200',
+      waterfall:'https://commons.wikimedia.org/wiki/Special:FilePath/Indonesian%20Waterfall.jpg?width=1200',
+      mountain:'https://commons.wikimedia.org/wiki/Special:FilePath/Mountain%20in%20west%20java.jpg?width=1200',
+      cloud:'https://commons.wikimedia.org/wiki/Special:FilePath/Awan%20kumulonimbus.jpg?width=1000',
+      sun:'https://commons.wikimedia.org/wiki/Special:FilePath/Sunset%20from%20Indonesia.jpg?width=1000',
+      soil:'https://commons.wikimedia.org/wiki/Special:FilePath/A%20soil.jpg?width=1000',
+      sunset:'https://commons.wikimedia.org/wiki/Special:FilePath/Sunset%20from%20Indonesia.jpg?width=1000',
+      experiment:'https://commons.wikimedia.org/wiki/Special:FilePath/Two%20Glasses%20of%20Water%20%28with%20cm%20Scale%29%20O.jpg?width=1000',
+      diagram:'https://commons.wikimedia.org/wiki/Special:FilePath/The%20Water%20Cycle.png?width=1100',
+      drought:'https://commons.wikimedia.org/wiki/Special:FilePath/Beautiful%20Landscape%20in%20Indonesia.jpg?width=1200',
+      rain:'https://commons.wikimedia.org/wiki/Special:FilePath/Awan%20kumulonimbus.jpg?width=1000',
+      forest:'https://commons.wikimedia.org/wiki/Special:FilePath/Pemandangan%20dari%20Atas%20Gunung.jpg?width=1200'
+    }
+  },
+  ecosystem:{
+    title:'Ekosistem', subtitle:'Menyelidiki hubungan makhluk hidup dan lingkungan di sawah.', accent:'eco',
+    stages:['Survei Sawah','Peran Komponen','Jaring Makanan','Populasi','Uji Gangguan','Misi Keseimbangan','Refleksi'],
+    hero:'https://commons.wikimedia.org/wiki/Special:FilePath/Rice%20Field%20Indonesia.jpg?width=1400',
+    images:{
+      field:'https://commons.wikimedia.org/wiki/Special:FilePath/Rice%20Field%20Indonesia.jpg?width=1400',
+      padi:'https://commons.wikimedia.org/wiki/Special:FilePath/Oryza%20sativa%20L.jpg?width=1000',
+      grass:'https://commons.wikimedia.org/wiki/Special:FilePath/Rice%20grasshopper.jpg?width=1000',
+      frog:'https://commons.wikimedia.org/wiki/Special:FilePath/Rice%20paddy%20frogs%20genus%20Fejervarya.jpg?width=1000',
+      bird:'https://commons.wikimedia.org/wiki/Special:FilePath/Sparrows%20in%20the%20rice%20field%20%2814749194072%29.jpg?width=1000',
+      fish:'https://commons.wikimedia.org/wiki/Special:FilePath/Fish%20Rice.jpg?width=1000',
+      water:'https://commons.wikimedia.org/wiki/Special:FilePath/Mina%20padi%20java%20Pj%20IMG-20150313-WA0004.jpg?width=1000',
+      soil:'https://commons.wikimedia.org/wiki/Special:FilePath/Rice%20Field%20Indonesia.jpg?width=1000',
+      harvest:'https://commons.wikimedia.org/wiki/Special:FilePath/Panen%20padi%20di%20sawah.jpg?width=1000',
+      balance:'https://commons.wikimedia.org/wiki/Special:FilePath/Paddy%20field%20in%20Kalibaru%2C%20East%20Java%2C%20Indonesia.jpg?width=1200',
+      riceplant:'https://commons.wikimedia.org/wiki/Special:FilePath/Ripening%20rice%20plants%20in%20paddy%20field%2C%20Indonesia.jpg?width=1100'
+    }
+  }
+};
